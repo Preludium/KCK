ca67e1aef252ecc21f056dc83a388baa2826d772
Mikolaj Szymczak <szymczak.mikolaj08@gmail.com>
2019-10-22 15:43:30 +0200

    update

--- lab3/piec_zero.py
+++ lab3/piec_zero.py
@@ -1,12 +1,9 @@
-import matplotlib
-import colorsys
 from matplotlib import pyplot as plt
-from matplotlib import colors
 
+map = []
 
 def main():
     pomap = []
-    map = []
     min_val = 0.432528741577922
     max_val = 1.5383204012924698
     with open('dane.txt', 'r') as f:
@@ -17,11 +14,15 @@ def main():
             for item in line.split():
                 pom.append((float(item) / 100 - min_val) / (max_val - min_val))
             pomap.append(pom)
-
-        for i in pomap:
+        dif = 0
+        for i in range(len(pomap)):
             pom = []
-            for j in i:
-                pom.append(get_gradient(j))
+            for j in range(1,len(pomap[i])):
+                shading = False
+                if (pomap[i][j-1] > pomap[i][j]):
+                    shading = True
+                    dif = 1 - (pomap[i][j-1] - pomap[i][j]) * 5
+                pom.append(get_gradient(pomap[i][j], shading, dif))
             map.append(pom)
 
     plt.imshow(map)
@@ -32,20 +33,30 @@ def hsv2rgb(h, s, v):
     return (h, s, v)
 
 
-def get_gradient(i):
-    if (i < 9/32):   # ziel
+def get_gradient(i, shading, dif):
+    if (i < 9/32):   # ziel\
         h = i * 2.5
         s = 1
         v = 0
+        if (shading):
+            h = h * dif 
+            s = s * dif
     elif (i < 1 / 2): # zolty
         h = 0.2 + i * 1.5
         s = 1
         v = 0 
+        if (shading):
+            h = h * dif
+            s = s * dif
     else:           # czerwony
-        h = 1
         s = 1 - (i - 1/2) * 2 
+        h = 1
         v = 0
+        if (shading):
+            s = s * dif
+            h = h * dif
     return hsv2rgb(h, s, v)
+
 
 if (__name__ == '__main__'):
     main()
